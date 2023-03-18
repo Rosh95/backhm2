1d34d7ccf160377af61691900680bc233b60edbf
Rosh1995 <azi.rovshan@gmail.com>
2023-03-19 00:02:32 +0300

    make second homework fixes

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,23 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
+exports.app = void 0;
 const testing_router_1 = require("./routes/testing-router");
-const port = process.env.port || 3000;
-app_1.app.use('/testing/all-data', testing_router_1.testRouter);
-app_1.app.listen(port, () => {
+const express_1 = __importDefault(require("express"));
+const blogs_router_1 = require("./routes/blogs-router");
+const posts_router_1 = require("./routes/posts-router");
+const port = process.env.port || 3001;
+exports.app = (0, express_1.default)();
+const parserMiddleWare = express_1.default.json();
+exports.app.use(parserMiddleWare);
+exports.app.use('/blogs', blogs_router_1.blogsRouter);
+exports.app.use('/posts', posts_router_1.postsRouter);
+exports.app.use('/testing/all-data', testing_router_1.testRouter);
+exports.app.use('/', (req, res) => {
+    res.send('Siiiiii');
+});
+exports.app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
-import {app} from './app';
 import {testRouter} from './routes/testing-router';
+import express, {Request, Response} from 'express';
+import {blogsRouter} from './routes/blogs-router';
+import {postsRouter} from './routes/posts-router';
 
-const port = process.env.port || 3000;
+const port = process.env.port || 3001;
+export const app = express();
 
+
+const parserMiddleWare = express.json()
+app.use(parserMiddleWare)
+
+
+app.use('/blogs', blogsRouter);
+app.use('/posts', postsRouter);
 app.use('/testing/all-data', testRouter);
+
+app.use('/', (req: Request, res: Response) => {
+    res.send('Siiiiii')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
