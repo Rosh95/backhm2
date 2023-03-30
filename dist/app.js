2a966086cd1398694b0067619737c17cc958cdec
Rosh1995 <azi.rovshan@gmail.com>
2023-03-31 00:02:14 +0300

    make second homework fixes

---
+++ dist/app.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
+const express_1 = __importDefault(require("express"));
+const blogs_router_1 = require("./routes/blogs-router");
+const posts_router_1 = require("./routes/posts-router");
+const testing_router_1 = require("./routes/testing-router");
+exports.app = (0, express_1.default)();
+const parserMiddleWare = express_1.default.json();
+exports.app.use(parserMiddleWare);
+exports.app.use('/blogs', blogs_router_1.blogsRouter);
+exports.app.use('/posts', posts_router_1.postsRouter);
+exports.app.use('/testing/all-data', testing_router_1.testRouter);
+exports.app.use('/', (req, res) => {
+    res.send('Siiiiii');
+});
